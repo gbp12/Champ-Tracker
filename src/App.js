295373586de64af1b63f1e0b4c788e26a65fef27
gbp12 <gonzabonilla4896@outlook.es>
2022-03-23 18:02:00 +0000

    eliminando codigo repetitivo

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ export const Campeones = (props) => {
 
 
 	async function fetchData() {
-		const response = await axios.get("http://ddragon.leagueoflegends.com/cdn/12.5.1/data/en_US/champion.json")
+		const response = await axios.get("https://ddragon.leagueoflegends.com/cdn/12.5.1/data/en_US/champion.json")
 		if (response.status === 200) {
 
 			setlista(response.data.data)
--- src/validations/ValidacionCampeon.js
+++
@@ -1,5 +0,0 @@
-import * as yup from "yup"
-
-export const userSchema = yup.object().shape({
-	nombre: yup.string().min(3).max(15),
-})
